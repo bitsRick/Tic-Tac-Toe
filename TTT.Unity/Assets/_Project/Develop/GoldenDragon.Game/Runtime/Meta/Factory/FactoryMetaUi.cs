6affb914041be337cf141234a3e9d493b146c038
BitRick <36227628+bitsRick@users.noreply.github.com>
2025-10-14 15:27:08 +1000

    add fabrica hud

---
+++ TTT.Unity/Assets/_Project/Develop/GoldenDragon.Game/Runtime/Meta/Factory/FactoryMetaUi.cs
@@ -0,0 +1,12 @@
+﻿namespace GoldenDragon._Project.Develop.GoldenDragon.Game.Runtime.Meta.Factory
+{
+    public class FactoryUi
+    {
+        public FactoryUi()
+        {
+            
+        }
+        
+        public void CreateMetaRoot
+    }
+}
